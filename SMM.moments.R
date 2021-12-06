6c103e18cd1409063e12e65196068688e240e111
mshardel <sike2001@yahoo.com>
2021-12-06 11:41:41 -0500

    fixed typos

--- SMM.moments.R
+++ SMM.moments.R
@@ -12,10 +12,10 @@
 
 ###moment functions (score equations) for G-estimation of structural mean models (SMMs)
 SMM.moments <- function(psi,x){
-             C <- x[,“C”] ###covariate
-             Y <- x[,“Y”] ###outcome
-             X <- x[,“X”] ###exposure
-             Z <- x[,“Z”] ###(possibly invalid) instrument
+             C <- x[,"C"] ###covariate
+             Y <- x[,"Y"] ###outcome
+             X <- x[,"X"] ###exposure
+             Z <- x[,"Z"] ###(possibly invalid) instrument
              ###predicted treatment-free counterfactual
              H <- Y*exp(-psi[1]*Z-psi[2]*X)
              ###q = E[H | C]
@@ -25,18 +25,18 @@ SMM.moments <- function(psi,x){
              D1 <- lm(dH.dpsi.1∼Z*C)$fitted.values
              dH.dpsi.2 <- -Y*X*exp(-psi[1]*Z-psi[2]*X)
              ###D2 = E[dH.dpsi.2 | Z, C]
-             D2 <- lm(dH.dpsi.2∼Z*C)$fitted.values
-             sq.dev <- (H-q)ˆ2
-             ###w = 1/E[(H-q)ˆ2 | Z, C]
-             w <- 1/(lm(sq.dev∼Z*C)$fitted.values)
+             D2 <- lm(dH.dpsi.2~Z*C)$fitted.values
+             sq.dev <- (H-q)^2
+             ###w = 1/E[(H-q)^2 | Z, C]
+             w <- 1/(lm(sq.dev~Z*C)$fitted.values)
              ###Ew = E[w | C]
              Ew <- lm(w∼C)$fitted.values
              wD1 <- w*D1
              ###EwD1 = E[wD1 | C]
-             EwD1 <- lm(wD1∼C)$fitted.values
+             EwD1 <- lm(wD1~C)$fitted.values
              wD2 <- w*D2
              ###EwD2 = E[wD2 | C]
-             EwD2 <- lm(wD2∼C)$fitted.values
+             EwD2 <- lm(wD2~C)$fitted.values
              ###Optimal weights for the estimating equations
              dopt.1 <- w*(D1 - EwD1/Ew)
              dopt.2 <- w*(D2 - EwD2/Ew)
